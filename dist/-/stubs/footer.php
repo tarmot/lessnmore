6a355c77429931d08ede7e9c11224634184b3080
Alan Hogan <contact@alanhogan.com>
2017-09-17 04:32:20 -0700

    Do not check for new LM versions when logged out

--- dist/-/stubs/footer.php
+++ dist/-/stubs/footer.php
@@ -1,6 +1,8 @@
 </div><!--#wrap-->
-<div id="footer-update-area" data-version="<?php echo BCURLS_VERSION ?>"></div>
-<script src="js/jquery-1.5.min.js"></script>
-<script src="js/version_check.js"></script>
+<?php if (user_is_logged_in()): ?>
+	<div id="footer-update-area" data-version="<?php echo BCURLS_VERSION ?>"></div>
+	<script src="js/jquery-1.5.min.js"></script>
+	<script src="js/version_check.js"></script>
+<?php endif; ?>
 </body>
 </html>
